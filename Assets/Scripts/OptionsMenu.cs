7a8fc11eb9063340c80f89fa70f64612bca03107
Abdullah Masood <mabd051104@gmail.com>
2025-12-09 22:11:43 +0500

    Add Volume option in Option Menu

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 public class OptionsMenu : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void setVolume(float volume)
     {
-        
+        AudioListener.volume = volume;
+        Debug.Log("Volume set to: " + volume);
     }
 
     // Update is called once per frame
